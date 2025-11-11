a68aabf9d16cd555c953ee2ba3123fc77565e12d
hazelgriffith <hazelgriffith30@gmail.com>
2025-11-11 15:09:14 -0500

    added evaluation tests

--- CaseStudies/Quadratic_Equation_Solver/QES_tester.py
+++ CaseStudies/Quadratic_Equation_Solver/QES_tester.py
@@ -3,13 +3,33 @@ import random
 import math
 
 '''
+accepts a string containing a complex number and removes negative signs from complex portion
+by changing the operator as appropriate to be compatible with the complex() python function
+args: string with form "a (+/-) (-)bj"
+returns: string with form "a (+/-) bj"
+'''
+def simplify_complex_num(number:str) -> str:
+    numberparts = number.split(" ")
+    if (len(numberparts) > 1):
+        if numberparts[2].startswith("-"):
+            numberparts[2] = numberparts[2].replace("-","").replace("\n","")
+            if numberparts[1] == "+":
+                numberparts[1] = "-"
+            else:
+                numberparts[1] = "+"
+        
+        return f"{numberparts[0]}{numberparts[1]}{numberparts[2]}"
+    else:
+        return number
+
+'''
 discriminant metamorphic relation tests
 d = b**2-4ac
 d > 0 => 2 roots
 d = 0 => 1 root
 d < 0 => 2 complex roots
 '''
-def disc_test_discriminant_check(a:int, b:int, c:int) -> bool:
+def test_discriminant(a:int, b:int, c:int) -> bool:
     d = (b**2)-(4*a*c)
     if d > 0:
         expRoots = "2 simple"
@@ -46,7 +66,7 @@ Checks if the Vieta sum and product relations between the roots and the coeffici
 Vieta's product: x1 * x2 = c/a
 Vieta's sum: x1 + x2 = -b/a
 '''
-def disc_test_vieta_check(a:int, b:int, c:int) -> bool:
+def test_vieta(a:int, b:int, c:int) -> bool:
 
     try:
         result = Quadratic.solveQuadratic(a, b, c)
@@ -55,28 +75,75 @@ def disc_test_vieta_check(a:int, b:int, c:int) -> bool:
 
         if len(result.split("\n")) > 1:
             line1,line2 = result.split("\n")
+            line2 = simplify_complex_num(line2.replace("x2 = ",""))
         else:
             line1 = result
-        #print(line1.replace("x1 = ","").replace(" ","").replace("\n",""))
-        x1 = complex(line1.replace("x1 = ","").replace(" ","").replace("\n",""))
+
+        line1 = simplify_complex_num(line1.replace("x1 = ",""))
+
+        x1 = complex(line1)
 
         if line2 != "":
-
-            x2 = complex(line2.replace("x2 = ","").replace(" ","").replace("\n",""))
+            x2 = complex(line2)
         
         if x2 is None:
             x2 = x1
 
         product = x1*x2
         sum = x1+x2
+
         assert(math.isclose(product.real,c/a)), "Vieta's product did not hold"
         assert(math.isclose(sum.real,-b/a)), "Vieta's sum did not hold"
         return True
+    
     except NotEnoughPrecisionException as e:
         print("Failed to find an accurate solution! This can happen when the values are too" +
 					" big, a is too close to zero, or b^2 is much bigger than 4ac.")
     return False
 
+'''
+Checks if the roots found, substituted into the quadratic formula equal zero
+ax**2 + bx**2 + c = 0
+'''
+def test_evaluation(a:int, b:int, c:int) -> bool:
+    try:
+        result = Quadratic.solveQuadratic(a, b, c)
+        line2 = ""
+        x2 = None
+
+        if len(result.split("\n")) > 1:
+            line1,line2 = result.split("\n")
+            line2 = simplify_complex_num(line2.replace("x2 = ",""))
+        else:
+            line1 = result
+
+        line1 = simplify_complex_num(line1.replace("x1 = ",""))
+
+        x1 = complex(line1)
+
+        if line2 != "":
+            x2 = complex(line2)
+        
+        if x2 is None:
+            x2 = x1
+
+        evalx1 = a*(x1**2)+b*x1+c
+        evalx2 = a*(x2**2)+b*x2+c
+
+        assert(math.isclose(evalx1.real,0,abs_tol=1e-9)), f"x1: {x1} is not a root of equation {a}x^2 + {b}x + {c}"
+        assert(math.isclose(evalx2.real,0,abs_tol=1e-9)), f"x2: {x2} is not a root of equation {a}x^2 + {b}x + {c}"
+
+        return True
+    
+    except NotEnoughPrecisionException as e:
+        print("Failed to find an accurate solution! This can happen when the values are too" +
+					" big, a is too close to zero, or b^2 is much bigger than 4ac.")
+    return False
+
+'''
+creates/runs seed tests for automatically generating/running test cases based on the
+metamorphic relations of discriminant properties
+'''
 def run_and_generate_discriminant_tests():
     k = random.randint(2,1000)
     #negative d
@@ -84,9 +151,9 @@ def run_and_generate_discriminant_tests():
     b = 5
     c = 5
     #seed test
-    seedResult = disc_test_discriminant_check(a,b,c)
+    seedResult = test_discriminant(a,b,c)
     #auto test (scale by k)
-    genResult = disc_test_discriminant_check(k*a,k*b,k*c)
+    genResult = test_discriminant(k*a,k*b,k*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
 
     k = random.randint(2,1000)
@@ -95,9 +162,9 @@ def run_and_generate_discriminant_tests():
     b = 5
     c = 1
     #seed test
-    seedResult = disc_test_discriminant_check(a,b,c)
+    seedResult = test_discriminant(a,b,c)
     #auto test (scale by k)
-    genResult = disc_test_discriminant_check(k*a,k*b,k*c)
+    genResult = test_discriminant(k*a,k*b,k*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     
     k = random.randint(2,1000)
@@ -106,11 +173,15 @@ def run_and_generate_discriminant_tests():
     b = 2
     c = 1
     #seed test
-    seedResult = disc_test_discriminant_check(a,b,c)
+    seedResult = test_discriminant(a,b,c)
     #auto test (scale by k)
-    genResult = disc_test_discriminant_check(k*a,k*b,k*c)
+    genResult = test_discriminant(k*a,k*b,k*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
 
+'''
+creates/runs seed tests for automatically generating/running test cases based on the
+metamorphic relations of the Vieta formulas for quadratic equations
+'''
 def run_and_generate_vieta_tests():
     k = random.randint(2,1000)
     #negative d
@@ -118,15 +189,15 @@ def run_and_generate_vieta_tests():
     b = 5
     c = 5
     #seed test
-    seedResult = disc_test_vieta_check(a,b,c)
+    seedResult = test_vieta(a,b,c)
     #auto test (scale by k)
-    genResult = disc_test_vieta_check(k*a,k*b,k*c)
+    genResult = test_vieta(k*a,k*b,k*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     #auto test (invert coefficients)
-    genResult = disc_test_vieta_check(c,b,a)
+    genResult = test_vieta(c,b,a)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     #auto test (scale by -1)
-    genResult = disc_test_vieta_check(-1*a,-1*b,-1*c)
+    genResult = test_vieta(-1*a,-1*b,-1*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
 
     k = random.randint(2,1000)
@@ -135,15 +206,15 @@ def run_and_generate_vieta_tests():
     b = 5
     c = 1
     #seed test
-    seedResult = disc_test_vieta_check(a,b,c)
+    seedResult = test_vieta(a,b,c)
     #auto test (scale by k)
-    genResult = disc_test_vieta_check(k*a,k*b,k*c)
+    genResult = test_vieta(k*a,k*b,k*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     #auto test (invert coefficients)
-    genResult = disc_test_vieta_check(c,b,a)
+    genResult = test_vieta(c,b,a)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     #auto test (scale by -1)
-    genResult = disc_test_vieta_check(-1*a,-1*b,-1*c)
+    genResult = test_vieta(-1*a,-1*b,-1*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     
     k = random.randint(2,1000)
@@ -152,19 +223,35 @@ def run_and_generate_vieta_tests():
     b = 2
     c = 1
     #seed test
-    seedResult = disc_test_vieta_check(a,b,c)
+    seedResult = test_vieta(a,b,c)
     #auto test (scale by k)
-    genResult = disc_test_vieta_check(k*a,k*b,k*c)
+    genResult = test_vieta(k*a,k*b,k*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     #auto test (invert coefficients)
-    genResult = disc_test_vieta_check(c,b,a)
+    genResult = test_vieta(c,b,a)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
     #auto test (scale by -1)
-    genResult = disc_test_vieta_check(-1*a,-1*b,-1*c)
+    genResult = test_vieta(-1*a,-1*b,-1*c)
     assert (seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
 
-
+'''
+Will generate 1 random quadratic equation and check if the 
+roots found for both it and its random transformations evaluate to zero
+It will repeat this for as many trials as provided
+low is the smallest possible number to be generated for coefficients, whereas high is the greatest
+args: low = int, high = int, trials = int
+'''
+def run_and_generate_evaluation_tests(low:int, high:int, trials:int):
+    for i in range(trials):
+        a = random.randint(low,high)
+        b = random.randint(low,high)
+        c = random.randint(low,high)
+        k = random.randint(low,high)
+        seedResult = test_evaluation(a,b,c)
+        genResult = test_evaluation(k*a,k*b,k*c)
+        assert(seedResult == genResult), f"seed test was: {seedResult}, generated test was: {genResult}"
 
 if __name__ == "__main__":
     run_and_generate_discriminant_tests()
     run_and_generate_vieta_tests()
+    run_and_generate_evaluation_tests(-1000,1000,5)
